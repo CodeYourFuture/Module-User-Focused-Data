5078f85d6753b251d6a045fa6a4b11419446e507
Armin <nouriarmin24@gmail.com>
2024-10-18 19:52:02 +0100

    i edit my css and and comment whole of my js and jquery

--- Form-Controls/index.js
+++ Form-Controls/index.js
@@ -1,33 +1,33 @@
-$(document).ready(function () {
-    // Event listener for input and textarea fields
-    $('form').find('input, textarea').on('keyup blur focus', function (e) {
-        var $this = $(this),
-            label = $this.prev('label');
+// $(document).ready(function () {
+//     // Event listener for input and textarea fields
+//     $('form').find('input, textarea').on('keyup blur focus', function (e) {
+//         var $this = $(this),
+//             label = $this.prev('label');
   
-        switch (e.type) {
-            case 'keyup':
-                if ($this.val() === '') {
-                    label.removeClass('active highlight');
-                } else {
-                    label.addClass('active highlight');
-                }
-                break;
+//         switch (e.type) {
+//             case 'keyup':
+//                 if ($this.val() === '') {
+//                     label.removeClass('active highlight');
+//                 } else {
+//                     label.addClass('active highlight');
+//                 }
+//                 break;
             
-            case 'blur':
-                if ($this.val() === '') {
-                    label.removeClass('active highlight');
-                } else {
-                    label.removeClass('highlight');
-                }
-                break;
+//             case 'blur':
+//                 if ($this.val() === '') {
+//                     label.removeClass('active highlight');
+//                 } else {
+//                     label.removeClass('highlight');
+//                 }
+//                 break;
             
-            case 'focus':
-                if ($this.val() === '') {
-                    label.removeClass('highlight');
-                } else if ($this.val() !== '') {
-                    label.addClass('highlight');
-                }
-                break;
-        }
-    });
-})
+//             case 'focus':
+//                 if ($this.val() === '') {
+//                     label.removeClass('highlight');
+//                 } else if ($this.val() !== '') {
+//                     label.addClass('highlight');
+//                 }
+//                 break;
+//         }
+//     });
+// })
